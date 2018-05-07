8d691931a3d494882219bb441a76c808e23400da
Mikhail Malyshev <mlshv42@ya.ru>
2018-05-07 19:30:56 +0300

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 const Home = () => (
@@ -31,22 +31,53 @@ const NotFound404 = () => (
     <p>Страница не найдена</p>
   </div>
 );
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <header>
-            <Link to="/">Главная</Link>
+            <NavLink
+              exact
+              activeStyle={{
+                color: '#000',
+                textDecoration: 'none',
+                cursor: 'default'
+              }}
+              to="/"
+            >
+              Главная
+            </NavLink>
             <span> / </span>
-            <Link to="/news">Новости</Link>
+            <NavLink
+              activeStyle={{
+                color: '#000',
+                textDecoration: 'none',
+                cursor: 'default'
+              }}
+              to="/news"
+            >
+              Новости
+            </NavLink>
             <span> / </span>
-            <Link to="/about">Про нас</Link>
+            <NavLink
+              activeStyle={{
+                color: '#000',
+                textDecoration: 'none',
+                cursor: 'default'
+              }}
+              to="/about"
+            >
+              Про нас
+            </NavLink>
           </header>
-          <Route path="/" component={Home} />
-          <Route path="/news" component={News} />
-          <Route path="/about" component={About} />
-          <Route component={NotFound404} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/news" component={News} />
+            <Route path="/about" component={About} />
+            <Route component={NotFound404} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
