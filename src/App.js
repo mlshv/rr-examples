17073544493e4e8a190e536a61c857dfb927193c
Gitlab CI <ci@cognitive.ru>
2018-05-07 18:34:18 +0300

    Код примера

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/">Главная</Link>
+            </li>
+            <li>
+              <Link to="/news">Новости</Link>
+            </li>
+            <li>
+              <Link to="/about">Про нас</Link>
+            </li>
+          </ul>
+          <Route
+            path="/"
+            render={() => (
+              <div>
+                <h1>Главная</h1>
+                <p>Это главная страница</p>
+              </div>
+            )}
+          />
+          <Route 
+            path="/news"
+            render={() => (
+              <div>
+                <h1>Новости</h1>
+                <p>Позавчера: заблокировали гугл</p>
+                <p>Вчера: заблокировали ютуб</p>
+                <p>Сегодня: телеграм работает</p>
+              </div>
+            )}
+          />
+          <Route 
+            path="/about"
+            render={() => (
+              <div>
+                <h1>Про нас</h1>
+                <p>Мы крутые</p>
+              </div>
+            )}
+          />
+          <Route 
+            render={() => (
+              <div>
+                <h1>404</h1>
+                <p>Not found</p>
+              </div>
+            )}
+          />
+        </div>
+      </BrowserRouter>
     );
   }
 }
